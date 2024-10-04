3aa099c522e73ea6efc5a54d23f10ea775d1eb83
Subash Jirel <subashjrl@gmail.com>
2024-10-04 10:22:47 +0545

    added functionality for adding form data of education to mock data

--- src/components/education/EducationSection.jsx
+++ src/components/education/EducationSection.jsx
@@ -1,17 +1,58 @@
 import { useState } from 'react';
 import InputGroup from '../inputGroup';
+import uniqid from 'uniqid';
+import { Trash2 } from 'lucide-react';
 function EducationSection({ education }) {
   const [showBtnForm, setShowBtnForm] = useState('button');
+  const [formData, setFormData] = useState({
+    degree: '',
+    schoolName: '',
+    startDate: '',
+    endDate: '',
+  });
   const btnShow = showBtnForm == 'button';
   const formShow = showBtnForm == 'form';
   function toggleShow() {
     setShowBtnForm((prev) => (prev == 'button' ? 'form' : 'button'));
   }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleEducationAdd() {
+    // Check for empty fields
+    if (
+      !formData.degree ||
+      !formData.schoolName ||
+      !formData.startDate ||
+      !formData.endDate
+    ) {
+      alert('All fields must be filled out');
+      return;
+    }
+    const newEducation = {
+      ...formData,
+      id: uniqid(),
+    };
+    // console.log(newEducation);
+    education.push(newEducation);
+    toggleShow();
+  }
   return (
     <>
       <h1 className="text-3xl">Education</h1>
       {education.map((val) => (
-        <div key={val.id}>{val.schoolName}</div>
+        <>
+          <div className="flex items-center">
+            <div className="m-3" key={val.id}>
+              {val.schoolName}{' '}
+            </div>
+            <Trash2 />
+          </div>
+        </>
       ))}
       {btnShow && (
         <button
@@ -29,6 +70,9 @@ function EducationSection({ education }) {
                 School Name:
               </label>
               <input
+                name="schoolName"
+                onChange={handleChange}
+                value={formData.schoolName}
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
                 type="text"
               />
@@ -36,6 +80,9 @@ function EducationSection({ education }) {
             <div className="mb-4">
               <label className="block text-sm font-bold mb-2">Degree:</label>
               <input
+                name="degree"
+                onChange={handleChange}
+                value={formData.degree}
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
                 type="text"
               />
@@ -45,6 +92,9 @@ function EducationSection({ education }) {
                 Start Date:
               </label>
               <input
+                name="startDate"
+                onChange={handleChange}
+                value={formData.startDate}
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
                 type="text"
               />
@@ -52,6 +102,9 @@ function EducationSection({ education }) {
             <div className="mb-4">
               <label className="block text-sm font-bold mb-2">End Date:</label>
               <input
+                name="endDate"
+                onChange={handleChange}
+                value={formData.endDate}
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
                 type="text"
               />
@@ -73,6 +126,7 @@ function EducationSection({ education }) {
               <button
                 type="button"
                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                onClick={handleEducationAdd}
               >
                 Save
               </button>
